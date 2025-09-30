d7db4e6092d03683a203c93f59b62b4f19b0371c
lantern-elf <lantern4k@gmail.com>
2025-09-30 17:23:23 +0700

    adding background

--- src/pages/Artworks.jsx
+++ src/pages/Artworks.jsx
@@ -6,15 +6,7 @@ const Artworks = () => {
   return (
     <>
       <Navbar artWorks={true} />
-      <main className='min-h-[92vh]'>
-        <section className='flex h-full flex-col items-center gap-4 lg:px-42 lg:py-5'>
-          <div className='flex w-[95%] justify-center'>
-            <img className='border' src="https://cdnb.artstation.com/p/assets/images/images/089/853/855/large/lantern-elf-shore.jpg?1752132886" alt="" />   
-          </div>
-          <div className='flex w-[95%] gap-3 justify-center'>
-            <img src="https://cdnb.artstation.com/p/assets/images/images/082/683/727/large/lantern-elf-blue-hour.jpg?1733662254" alt="" />
-          </div>
-        </section>
+      <main className='h-fit' >
       </main>
       <Footer />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,16 +7,17 @@ const Home = () => {
     <>
       <Navbar home={true} />
       <main className='min-h-[92vh]'>
-        <section className="justify-end flex flex-col bg-center bg-cover lg:grid lg:h-[92vh] lg:place-content-center">
+        {/* Landing Start */}
+        <section className="bg-[url(/public/bg.png)] justify-end flex flex-col bg-center bg-cover lg:grid lg:h-[92vh] lg:place-content-center">
           <div className="mx-auto w-screen max-w-screen-xl px-4 py-16 lg:px-8 lg:py-32">
-            <div className="max-w-prose text-left">
-              <h1 className="text-5xl font-bold text-black text-shadow-2xs sm:text-7xl">
-                Lantern's Portfolio
+            <div className="max-w-[50%]">
+              <h1 className="text-5xl font-bold text-black text-shadow-2xs sm:text-7xl text-right">
+                Lantern Elf's Portfolio
               </h1>
-              <p className="mt-4 text-base text-pretty text-shadow-2xs text-black sm:text-lg/relaxed">
-                2D Artist, Concept Artist & Programmer
+              <p className="mt-4 text-base text-pretty text-shadow-2xs text-black sm:text-lg/relaxed text-right">
+                2D Artist & Programmer
               </p>
-              <div className="mt-4 flex gap-4 sm:mt-6">
+              <div className="mt-4 flex gap-4 sm:mt-6 justify-end">
                 <a className="inline-block rounded cursor-pointer bg-accent px-3 py-2 font-medium transition-color shadow">
                   Explore
                 </a>
@@ -24,6 +25,8 @@ const Home = () => {
             </div>
           </div>
         </section>
+        {/* Landing End */}
+        
       </main>
       <Footer />
     </>
