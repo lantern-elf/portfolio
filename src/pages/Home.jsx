bb5603522f60644730afbd53ca035bb4a33736de
lantern-elf <lantern4k@gmail.com>
2025-11-15 15:18:26 +0700

    Fix image source path in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <main className='min-h-[94vh]'>
         {/* Landing Start */}
         <section className="bg-[url(/public/image/bg.webp)] justify-start flex flex-col bg-center bg-cover h-[94vh] md:place-content-center">
-          <img className='hidden md:block lg:w-100 w-70 relative md:top-55 md:left-20' src="public/image/title.png" alt="" />
+          <img className='hidden md:block lg:w-100 w-70 relative md:top-55 md:left-20' src="image/title.png" alt="" />
         </section>
         {/* Landing End */}
       </main>
