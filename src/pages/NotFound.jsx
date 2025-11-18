1e6a15a83b50ae6631dd962037d231942f683f2d
lantern-elf <lantern4k@gmail.com>
2025-11-18 09:24:24 +0700

    Add robots.txt to allow all user agents and specify sitemap location

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -8,7 +8,7 @@ const NotFound = () => {
       <Navbar />
       <main class="grid place-items-center min-h-[94vh]">
         <div className="text-center">
-          <p className="text-9xl font-semibold text-accent">404</p>
+          <p className="text-9xl font-semibold">404</p>
           <h1 className="mt-2 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-4xl">Page not found</h1>
         </div>
       </main>
